9df663388aacbfe4fca2cc247aa48bab83d71178
SiCong Li <meneerli17@gmail.com>
2024-03-03 16:11:40 +0000

    Publish post_5. Update README with other interesting blogs

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css")
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addWatchTarget("./src/sass/");
 
   eleventyConfig.addFilter('firstCommitDate', function (file) {
